a5df383d3501429d48dc6695b365c27ede214a1f
tracysgit <tracy@wildiris.net>
2024-01-16 10:35:42 -0600

    Added 'name' attribute to mobile menu element.

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -12,13 +12,12 @@ function Header() {
     <header>
       <h1 className="h1-headline"><Link to="/work"><span className="visually-hidden">Artwork by </span>Seanne Buoy</Link></h1>
       <nav className="mainnav" aria-label="Main Menu">
-        <input className="mainmenu-btn" type="checkbox" id="mainmenu-btn" />
+        <input className="mainmenu-btn" type="checkbox" id="mainmenu-btn" name="mainmenu-btn" />
         <label className="mainmenu-icon" htmlFor="mainmenu-btn" tabIndex="0"><span className="navicon"></span></label>
         <ul className="mainmenu">
           <li><NavLink to="/">Home</NavLink></li>
           <li><NavLink to="/work">Work</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
-          {/* <li><NavLink to="/features">Features</NavLink></li> */}
           <li><LinkContactUs social={SOCIAL} text={"Contact"} /></li> 
         </ul>
       </nav>
